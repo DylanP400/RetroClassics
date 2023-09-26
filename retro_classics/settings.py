70c78acefff24fb4ca918f12f34956483312c5ce
Dylan <Dylanmcmahon187@gmail.com>
2023-09-26 15:18:55 +0000

    Add background image and callout to homepage

--- retro_classics/settings.py
+++ retro_classics/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dylanp400-retroclassics-lhapwo8bmm9.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-dylanp400-retroclassics-lhapwo8bmm9.ws-eu105.gitpod.io']
 
 
 # Application definition
