ff1cb8d66649db52b94e175ebd999945a90738e5
Dylan <Dylanmcmahon187@gmail.com>
2023-10-23 20:04:49 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
 from .forms import OrderForm
+from django.conf import settings
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         message.error(request, "Your bag is empty right now")
@@ -13,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'stripe_secret_key': stripe_secret_key,
     }
 
     return render(request, template, context)
