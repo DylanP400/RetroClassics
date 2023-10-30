f8809ba454c3961370c5158e54fbc249c6620a44
Dylan <Dylanmcmahon187@gmail.com>
2023-10-30 01:14:59 +0000

    Add products for sale and testimonials on the home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ view to return the index page """
-    return render(request, 'home/index.html')
+    sale_products = Product.objects.filter(on_sale=True)[:4]
+    return render(request, 'home/index.html', {'sale_products': sale_products})
