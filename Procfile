e29d55459e9de81341d570f9b43fdeb5ea0d133c
Dylan <Dylanmcmahon187@gmail.com>
2023-10-30 02:28:11 +0000

    Add database and now about to push to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn retro-classics.wsgi:application
--- retro_classics/settings.py
+++ retro_classics/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 if os.path.isfile('env.py'):
     import env
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dylanp400-retroclassics-lhapwo8bmm9.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['localhost', 'dylanp400-retro-classics.herokuapp.com']
 
 
 # Application definition
@@ -125,13 +126,17 @@ WSGI_APPLICATION = 'retro_classics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
