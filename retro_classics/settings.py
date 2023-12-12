16b189447f210d56993ba804a17d349cf3ed6900
Dylan <Dylanmcmahon187@gmail.com>
2023-12-12 21:24:29 +0000

    Set debug to false

--- retro_classics/settings.py
+++ retro_classics/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 # Debug commented out because it gave a bad request 400 error and server would not start.
 
