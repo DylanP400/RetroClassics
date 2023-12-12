00e65827a3d1c505a4e31e579eb3fa4c079cb916
Dylan <Dylanmcmahon187@gmail.com>
2023-12-12 21:16:34 +0000

    Switched to ElephantSQL database

--- retro_classics/settings.py
+++ retro_classics/settings.py
@@ -135,17 +135,18 @@ WSGI_APPLICATION = 'retro_classics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-
-
+# ElephantSQL database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
