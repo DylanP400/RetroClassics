e7d9fef109a83696057337bc2d3b6328b420de23
Dylan <Dylanmcmahon187@gmail.com>
2024-02-29 17:15:15 +0000

    Add testimonials to home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from products.models import Product
+from testimonials.models import UserTestimonial
 
 
 def index(request):
     """ view to return the index page """
     sale_products = Product.objects.filter(on_sale=True)[:4]
-    return render(request, 'home/index.html', {'sale_products': sale_products})
+    testimonials = UserTestimonial.objects.all().order_by('-date_created')[:3]
+
+    context = {
+        'sale_products': sale_products,
+        'testimonials': testimonials,
+        }
+    return render(request, 'home/index.html', context)
