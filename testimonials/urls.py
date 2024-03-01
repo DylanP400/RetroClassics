f5a9a6047c27df9d4b8523f3dc846ead1b328642
Dylan <Dylanmcmahon187@gmail.com>
2024-03-01 00:12:10 +0000

    Add edit & delete for testimonials

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -3,6 +3,8 @@ from .views import (
     UserTestimonialsListView,
     UserTestimonialsDetailView,
     add_testimonial,
+    edit_testimonial,
+    delete_testimonial,
 )
 from . import views
 
@@ -11,7 +13,17 @@ path('add_testimonial/', views.add_testimonial, name='add_testimonial'),
 path(
     'UserTestimonials/<int:pk>/',
     UserTestimonialsDetailView.as_view(),
-    name='testimonials-detail'
+    name='testimonials_detail'
     ),
+path(
+    'edit_testimonial/<int:pk>',
+    views.edit_testimonial.as_view(),
+    name='edit_testimonial'
+    ),
+     path(
+        'delete_testimonial/<int:pk>',
+        views.delete_testimonial.as_view(),
+        name='delete_testimonial'
+        ),
 path('', UserTestimonialsListView.as_view(), name='home-page'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.contrib.auth.decorators import login_required
 from .models import UserTestimonial
 from .forms import TestimonialForm
 from django.contrib import messages
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 
@@ -40,6 +48,45 @@ def add_testimonial(request):
     else:
         form = TestimonialForm()
     context = {
-        'form': form
+        'form': form,
     }
     return render(request, 'add_testimonial.html', context)
+
+
+class edit_testimonial(UserPassesTestMixin, UpdateView):
+    """
+    View for updating a Testimonial.
+    """
+    model = UserTestimonial
+    fields = ['content', 'rating']
+
+    def test_func(self):
+        testimonial = self.get_object()
+        return self.request.user == testimonial.user
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(
+            self.request,
+            'You have successfully updated your testimonial'
+            )
+        return response
+
+    def get_success_url(self):
+        return reverse('edit_testimonial', args=[self.object.pk])
+
+
+class delete_testimonial(UserPassesTestMixin, DeleteView):
+    """
+    View for deleting a Testimonial.
+    """
+    model = UserTestimonial
+    success_url = reverse_lazy('profile')
+
+    def test_func(self):
+        testimonial = self.get_object()
+        return self.request.user == testimonial.user
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'You have successfully deleted your testimonial')
+        return super().delete(request, *args, **kwargs)
